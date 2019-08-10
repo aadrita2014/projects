3b1500797eee265684746530a82bd66215c58216
Ricky <bhavesh.bansal@gmail.com>
2019-08-10 13:40:53 +0530

    Login page completed

---
+++ UniCon/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UniCon
+//
+//  Created by Ricky on 8/10/19.
+//  Copyright © 2019 Rick. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        //Add Default Background Color to the view
+        self.view.addDefaultBackgroundColor()
+        
+    }
+
+
+}
+
