53f56c27d2c7353d940d8734c59d2c3cb9101c54
Ricky <bhavesh.bansal@gmail.com>
2019-08-10 12:47:12 +0530

    Splash Screen Completed

---
+++ UniCon/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  UniCon
+//
+//  Created by Ricky on 8/10/19.
+//  Copyright © 2019 Rick. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addDefaultBackgroundColor()
+    {
+        self.backgroundColor = UIColor(red: 3.0/255.0, green: 15.0/255.0, blue: 18.0/255.0, alpha: 1)
+    }
+}
--- UniCon/ViewController.swift
+++ UniCon/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        //Add Default Background Color to the view
+        self.view.addDefaultBackgroundColor()
+        
     }
 
 
