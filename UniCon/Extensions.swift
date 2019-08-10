5a0f0c794e3f29a92842e9213ff89356168d1a38
Ricky <bhavesh.bansal@gmail.com>
2019-08-10 13:49:08 +0530

    Login design completed

--- UniCon/Extensions.swift
+++ UniCon/Extensions.swift
@@ -14,3 +14,11 @@ extension UIView {
         self.backgroundColor = UIColor(red: 3.0/255.0, green: 15.0/255.0, blue: 18.0/255.0, alpha: 1)
     }
 }
+
+extension UIButton {
+    func underline() {
+        let attributedString = NSMutableAttributedString(string: (self.titleLabel?.text!)!)
+        attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: (self.titleLabel?.text!.count)!))
+        self.setAttributedTitle(attributedString, for: .normal)
+    }
+}
--- UniCon/LoginViewController.swift
+++ UniCon/LoginViewController.swift
@@ -10,14 +10,39 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    //IBOutlets Declared
+    @IBOutlet weak var loginButton:UIButton!
+    @IBOutlet weak var signupButton:UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
         //Add Default Background Color to the view
         self.view.addDefaultBackgroundColor()
         
+        //Underline the buttons
+        loginButton.underline()
+        signupButton.underline()
+        
+    }
+    @IBAction func emailLoginClicked()
+    {
+        print("Email Login Clicked")
     }
 
-
+    @IBAction func kakaoLoginClicked()
+    {
+        print("Kakao Login Clicked")
+    }
+    
+    @IBAction func facebookLoginClicked()
+    {
+        print("Facebook Login Clicked")
+    }
+    @IBAction func googleLoginClicked()
+    {
+        print("Google Login Clicked")
+    }
 }
 
