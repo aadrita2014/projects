77bd5b00f6d7f0a5952034e92510dd6f3fe3b806
Ricky <bhavesh.bansal@gmail.com>
2019-08-20 22:23:00 +0530

    Partially Completed New Screen 1

--- UniCon/Helpers/Extensions.swift
+++ UniCon/Helpers/Extensions.swift
@@ -33,6 +33,16 @@ extension UIButton {
 
 extension UITextField {
     
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
     
     func addBottomBorder(color:UIColor = UIColor.white){
         let bottomLine = CALayer()
