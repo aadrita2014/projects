99e5616eecba4b64c75df564474f5ffc34924857
Ricky <bhavesh.bansal@gmail.com>
2019-08-30 18:52:45 +0530

    Contest Deposited Screen Completed

---
+++ UniCon/Client VC/ContestDepostiConfirmationVC.swift
@@ -0,0 +1,33 @@
+//
+//  ContestDepostiConfirmationVC.swift
+//  UniCon
+//
+//  Created by Ricky on 8/30/19.
+//  Copyright © 2019 Rick. All rights reserved.
+//
+
+import UIKit
+
+class ContestDepostiConfirmationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //Add default black background color
+        self.view.addBlackBackgroundColor()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
