2a2ce33505e8a8ef66f0b62313456b0171ac6549
Ricky <bhavesh.bansal@gmail.com>
2019-08-30 18:34:16 +0530

    Image picker completed

--- UniCon/Client VC/ClientContestRegDetailsViewController.swift
+++ UniCon/Client VC/ClientContestRegDetailsViewController.swift
@@ -20,10 +20,9 @@ class ClientContestRegDetailsViewController: UIViewController {
     @IBOutlet weak var imageCollView: UICollectionView!
     
     @IBOutlet weak var infoDescTv: UITextView!
-    var images: [String] = ["demo_featured_creaor",
-                               "demo_featured_creaor",
-                               "demo_featured_creaor",
-    ]
+    var images: [String] = []
+    
+    var imgPicker:UIImagePickerController?
     //MARK: Overriden view methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +34,8 @@ class ClientContestRegDetailsViewController: UIViewController {
         
         //Other view setup
         viewSetup()
+        
+        
     }
     
     //MARK: Other View Setup
@@ -63,6 +64,18 @@ class ClientContestRegDetailsViewController: UIViewController {
         //Text view setup
         infoDescTv.addBorderColor(borderWidth: 1.0, color: UIColor.white)
         infoDescTv.addCornerRadius()
+        
+        //Async setup of the image controller
+        DispatchQueue.main.async {
+            self.imgPicker = UIImagePickerController()
+            self.imgPicker?.sourceType = .savedPhotosAlbum
+            self.imgPicker?.allowsEditing = false
+            self.imgPicker?.delegate = self
+        }
+    }
+    
+    func openPhotoPicker() {
+        present(imgPicker!, animated: true, completion: nil)
     }
 }
 
@@ -70,13 +83,45 @@ class ClientContestRegDetailsViewController: UIViewController {
 extension ClientContestRegDetailsViewController:UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
  
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count
+        //MAX Images user can add
+        return 3
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RefImageCell", for: indexPath) as! RefImageCell
+        
+        //Custom actions defined in RegImageCell
+        cell.addAction = { cell in
+            self.openPhotoPicker()
+        }
+        cell.delAction = { cell in
+            self.images.remove(at: indexPath.row)
+            collectionView.reloadData()
+        }
+        //If image is added then hide/show views in the collection view accordingly
+        if images.count > indexPath.row {
+            cell.refImage.image = UIImage(named: images[indexPath.row])
+            cell.addImgBtn.isHidden = true
+            cell.deletebtn.isHidden = false
+        }
+        else {
+            cell.addImgBtn.isHidden = false
+            cell.deletebtn.isHidden = true
+        }
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.viewWidth()/6, height: collectionView.viewWidth()/6)
+    }
+}
+
+extension ClientContestRegDetailsViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print(info)
+        images.append("demo_featured_creator")
+        imageCollView.reloadData()
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
