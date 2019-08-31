fedc3164983920107b6f364eff0b8b65c074ae44
Ricky <bhavesh.bansal@gmail.com>
2019-08-31 14:30:49 +0530

    Client Registration Confirmation Completed

--- UniCon/Client VC/ClientRegistrationConfirmationVC.swift
+++ UniCon/Client VC/ClientRegistrationConfirmationVC.swift
@@ -14,17 +14,9 @@ class ClientRegistrationConfirmationVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //Add black background color
+        self.view.addBlackBackgroundColor()
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
