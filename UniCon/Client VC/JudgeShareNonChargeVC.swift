f8b5df967b8a97a2193be737d63e235389ec2724
Ricky <bhavesh.bansal@gmail.com>
2019-08-31 16:23:26 +0530

    Contest Details Shared Deposit Confirmation Completed

---
+++ UniCon/Client VC/JudgeShareNonChargeVC.swift
@@ -0,0 +1,90 @@
+//
+//  JudgeShareNonChargeVC.swift
+//  UniCon
+//
+//  Created by Ricky on 8/31/19.
+//  Copyright © 2019 Rick. All rights reserved.
+//
+
+import UIKit
+
+class JudgeShareNonChargeVC: UIViewController {
+    
+    
+    //MARK: IBOutlets Declaration
+    @IBOutlet weak var numberOfParticipantsLabel: UILabel!
+    @IBOutlet weak var bgView1: UIView!
+    @IBOutlet weak var bgView2: UIView!
+    @IBOutlet weak var bgView3: UIView!
+    @IBOutlet weak var bgView4: UIView!
+    
+    @IBOutlet weak var shareChargeBtn: UIButton!
+    @IBOutlet weak var changeBtn: UIButton!
+    
+    @IBOutlet weak var currentShareFeeTf: UITextField!
+    @IBOutlet weak var costPerShareTf: UITextField!
+    @IBOutlet weak var shareCostExhaustAmtTf: UITextField!
+    @IBOutlet weak var reachWhenChargingShareTf: UITextField!
+    
+    
+    //MARK: Overriden view methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Add Default Background Color to the view
+        self.view.addBlackBackgroundColor()
+        
+        //Other Setup View
+      //  setupView()
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        setupView()
+    }
+    func setupView()   {
+        
+        //To be used when API will be integrated
+        //        let attributedString = NSMutableAttributedString(string: "현재 10,000,000명이\n콘테스트 영상을 공유중입니다.", attributes: [
+        //            .font: UIFont(name: "NotoSansCJKkr-Regular", size: 16.0)!,
+        //            .foregroundColor: UIColor.white
+        //            ])
+        //        attributedString.addAttributes([
+        //            .font: UIFont(name: "NotoSansCJKkr-Bold", size: 32.0)!,
+        //            .foregroundColor: AppColors.default_red_color
+        //            ], range: NSRange(location: 3, length: 11))
+        //
+        //        numberOfParticipantsLabel.attributedText = attributedString
+        
+        //Background views setup
+        bgView1.addDarkGrayBackgroundColor()
+        bgView1.addCornerRadius()
+        
+        bgView2.addDarkGrayBackgroundColor()
+        bgView2.addCornerRadius()
+        
+        bgView3.addDarkGrayBackgroundColor()
+        bgView3.addCornerRadius()
+        
+        bgView4.addDarkGrayBackgroundColor()
+        bgView4.addCornerRadius()
+        
+        //Buttons setup
+        shareChargeBtn.addCornerRadius()
+        changeBtn.addCornerRadius()
+        
+        //Textfields setup
+        currentShareFeeTf.updatePlaceHolder(text: "000", color: AppColors.default_red_color)
+        currentShareFeeTf.addBottomBorder(color: UIColor.lightGray)
+        
+        costPerShareTf.updatePlaceHolder(text: "000", color: AppColors.default_red_color)
+        costPerShareTf.addBottomBorder(color: UIColor.lightGray)
+        
+        shareCostExhaustAmtTf.updatePlaceHolder(text: "000", color: AppColors.default_red_color)
+        shareCostExhaustAmtTf.addBottomBorder(color: AppColors.default_red_color)
+        
+        reachWhenChargingShareTf.updatePlaceHolder(text: "000", color: AppColors.default_red_color)
+        reachWhenChargingShareTf.addBottomBorder(color: UIColor.lightGray)
+        
+    }
+    
+}
