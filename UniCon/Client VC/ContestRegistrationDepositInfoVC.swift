e2d446ab5100635c3aa602f21d90eca0ed04bada
Ricky <bhavesh.bansal@gmail.com>
2019-08-31 14:51:42 +0530

    Contest Reg Deposit Info Completed

---
+++ UniCon/Client VC/ContestRegistrationDepositInfoVC.swift
@@ -0,0 +1,23 @@
+//
+//  ContestRegistrationDepositInfoVC.swift
+//  UniCon
+//
+//  Created by Ricky on 8/31/19.
+//  Copyright © 2019 Rick. All rights reserved.
+//
+
+import UIKit
+
+class ContestRegistrationDepositInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //Add black background color
+        self.view.addBlackBackgroundColor()
+        
+    }
+
+}
---
+++ UniCon/Client VC/FinishedAppointmentVC.swift
@@ -0,0 +1,24 @@
+//
+//  FinishedAppointmentVC.swift
+//  UniCon
+//
+//  Created by Ricky on 8/31/19.
+//  Copyright © 2019 Rick. All rights reserved.
+//
+
+import UIKit
+
+class FinishedAppointmentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //Add black background color
+        self.view.addBlackBackgroundColor()
+    }
+    
+    
+    
+    
+    
+}
