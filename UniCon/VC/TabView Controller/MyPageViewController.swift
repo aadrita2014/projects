f21cdca7d915539b4228a8baf13ef636220f00f1
Gaurav Dixhit <gaurav.dixhit@appsierra.com>
2019-11-05 11:28:18 +0530

    My Profile Started

--- UniCon/VC/TabView Controller/MyPageViewController.swift
+++ UniCon/VC/TabView Controller/MyPageViewController.swift
@@ -18,16 +18,29 @@ class MyPageViewController: UIViewController {
     @IBOutlet weak var tableView:UITableView!
       
     
-    var options:[Menu] {
+    var menuOptions:[Menu] {
         get {
             if let user =  TokenManager.loggedInUser, user.role == Role.client.rawValue {
-                
+                return clientMenu
             }
             else {
-                
+                return creatorMenu
             }
         }
     }
+    
+    var creatorMenu:[Menu] = [
+        Menu(title: "개인정보 수정", segue: "EditPersonal", showDetail: true),
+        Menu(title: "나의 상금",segue: "MyPrize",showDetail: true),
+        Menu(title: "나의 뱃지", segue: "MyBadge", showDetail: true),
+        Menu(title: "지원센터", segue: "SupportCenter", showDetail: true),
+        Menu(title: "이용약관", segue: "TermsUse", showDetail: true),
+        Menu(title: "개인정보 처리방침", segue: "PrivacyPolicy", showDetail: true),
+        Menu(title: "유니콘 정보", segue: "UniconInfo", showDetail: true),
+        Menu(title: "로그아웃", segue: "", showDetail: false)
+    ]
+    
+    var clientMenu:[Menu] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,15 +48,42 @@ class MyPageViewController: UIViewController {
         //Add Default Background Color to the view
         self.view.addDefaultBackgroundColor()
         // Do any additional setup after loading the view.
+        viewSetup()
     }
-    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    func viewSetup() {
+        tableView.tableFooterView = UIView(frame: .zero)
     }
 }
 
 extension MyPageViewController:UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return menuOptions.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath)
+        
+        let label = cell.viewWithTag(1) as! UILabel
+        let detailIcon = cell.viewWithTag(2) as! UIImageView
+        let menu = menuOptions[indexPath.row]
+        label.text = menu.title
+        detailIcon.isHidden = !menu.showDetail
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let menu = menuOptions[indexPath.row]
+        if !menu.segue.isEmpty {
+           // self.performSegue(withIdentifier: menu.segue, sender: nil)
+        }
+        else {
+            
+        }
+    }
+    
+    
 }
